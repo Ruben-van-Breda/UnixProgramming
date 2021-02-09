ab89fb71ae17e564de783bfd75bf04fb04befa1a
RubenVanBreda <re.vanbreda@gmail.com>
2021-02-09 18:19:32 +0200

    worked in adding argument -n for the number of lines

--- assign1-19200704.c
+++ assign1-19200704.c
@@ -9,15 +9,17 @@
 #include <getopt.h>
 
 void print_manaul();
-void head_of_file();
-void tail_of_file();
+void head_of_file(int n);
+void tail_of_file(int n);
 
 int main(int argc, char **argv)
 {
     int option;
     int hflag; // header flag
     int tflag; // tail flag
-    while ((option = getopt(argc, argv, "h:tv")) != -1)
+    int kflag;
+    int numOfLines = 10;
+    while ((option = getopt(argc, argv, "h:n:t:v")) != -1)
     {
         switch (option)
         {
@@ -25,21 +27,26 @@ int main(int argc, char **argv)
                 if(hflag){
                     print_manaul();
                 }else{
-                    hflag++; tflag++; // increase to avoid errors in arguments
+                    hflag++; tflag++; kflag++;// increase to avoid errors in arguments
                 }
-                head_of_file(2);
+                head_of_file(numOfLines);
                 break;
             case 't':
                 if(tflag){
                     print_manaul();
                 }else{
-                    hflag++; tflag++; // increase to avoid errors in arguments
+                    hflag++; tflag++; kflag++;// increase to avoid errors in arguments
                 }
-                tail_of_file();
+                tail_of_file(numOfLines);
                 break;
 
             case 'v':
                 printf("Ruben van Breda,\truben.vanbreda@ucdconnect.ie,\t19200704\n");
+                break;
+            case 'n':
+                
+                numOfLines = atoi(optarg);
+                
                 break;
             default:
                 printf("Error");
@@ -61,18 +68,14 @@ void head_of_file(int n){
     size_t len = 0;
     ssize_t read;
 
+    if(optarg == 0){
+         printf("File could not be opened.");
+    }
+    // has file argument
     fp = fopen(optarg,"r");
     if(fp != NULL){
         printf("Head of file:\n");
-        // //loop thru characters in file
-        // while( (cur_char = fgetc(fp)) != EOF){
-        //     printf("%c",cur_char);
-        //     n--;
-        //     if(n<=0){
-        //         break;
-        //     }
-        // }
-
+    
         while((read = getline(&line,&len,fp)) != -1){
             printf("%s",line);
             n--;
@@ -85,8 +88,34 @@ void head_of_file(int n){
     
 }
 
-void tail_of_file(){
+void tail_of_file(int n){
     printf("Tail of file:");
+    
+    FILE *fp;
+    char *file_name;
+    char cur_char;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int counter = 0;
+    if(optarg == 0){
+         printf("File could not be opened.");
+    }
+    // has file argument
+    fp = fopen(optarg,"r");
+    if(fp != NULL){
+        printf("Head of file:\n");
+    
+        while((read = getline(&line,&len,fp)) != -1){
+            printf("%s",line);
+            n--;
+            if(n<= 0){break;}
+        }
+    }
+    else{
+        printf("File could not be opened.");
+    }
+    
 }
 
 
