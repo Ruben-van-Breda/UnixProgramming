f81a1bb40fb1cb07bbcc290ad6ec29df013ecd08
RubenVanBreda <re.vanbreda@gmail.com>
2021-05-04 16:13:13 +0200

    Assignment 4 Complete

--- Assignment 4/assign4_19200704.sh
+++ Assignment 4/assign4_19200704.sh
@@ -2,28 +2,48 @@
 
 
 
-# Check that we have the correct arguments
-if [ "$#" != 2 ] 
-then
-    echo "Invalid arguments"
-    exit 1;
-fi
+INFO="Created by Ruben van Breda, 19200704, email: ruben.vanbreda@ucdconnect.ie
 
-# # Check that the source directory is valid
-# if [ -d $1 ]; then
-#     echo "Invalid source directory: $1"
-#     exit -1
-# fi
-# # Check that the destination directory is valid
-# if [ -d $2 ]; then
-#     echo "Invalid destination directory: $2"
-#     exit -1
-# fi
+This bash script takes 2 arguments a source folder and a destination folder
+It will copy all the .png files and convert it to jpg into the destination maintainning the source hierarchy
+Dependencies
+* imagemagick - convert a png to jpg
+* 'brew install imagemagick'
 
+EXECUTION:
+./assign4_19200704 [source directory] [destination directory]
+"
 source=$1
 dest=$2
 
-# Define function
+# Check if a file is a .png if so convert it to .jpg and copy it into destination corrosponding directory
+ConvertAndMoveFile(){
+    # Parse data
+    FULLNAME=`echo "${1}" | sed "s/.*\///" ` 
+    NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
+    EXTENSION=`echo "$1" | cut -d'.' -f2`
+    THEPATH="${1/$source}";
+    THEPATH="${THEPATH/$FULLNAME}"
+
+    # Check if the file extension is .png
+    case  ${EXTENSION} in 
+        "png")
+            #Check if the directory doesn't exits (basicly any sub dir's)
+            # if [ ! -d "$dest$THEPATH" ]; then
+            #     mkdir -p "$dest$THEPATH" 
+            # fi
+            [ -d "$dest$THEPATH" ] || mkdir -p "$dest$THEPATH" 
+            # Convert .png to .jpg and store in destination
+            convert "$1" "$dest$THEPATH/$NAME.jpg";
+
+            ;;
+        *)
+        # if its not a .png do nothing
+        ;;  
+    esac
+    return;
+}
+# Search Directors function
 searchDir() {
  		
  	
@@ -32,52 +52,57 @@ searchDir() {
         FULLNAME=`echo "${file}" | sed "s/.*\///" ` 
         NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
         EXTENSION=`echo "$file" | cut -d'.' -f2`
-        
         THEPATH="${1/$source}";
-
-       echo "Path : $THEPATH"
         #recursivly look inside inner directories
         if [ -d "$file" ]; then
             searchDir "$file"
-        fi
 
-        case  ${EXTENSION} in 
-        "png")
-           # echo "PNG FILE : $PATH"
-            echo "$1/$FULLNAME -> " "$dest$THEPATH/$FULLNAME"
-            mkdir -p "$dest$THEPATH/" 
-            cp "$1/$FULLNAME" "$dest$THEPATH/$FULLNAME"
-            ;;
-        
-        *)
-        echo "$FULLNAME"
-        ;;  
-    esac
+        else
+            ConvertAndMoveFile "$file"
+        fi
 done
- 		# Return returns an exit status number between 0 - 255
- 		return
+# Return returns an exit status number between 0 - 255
+return 1
 }
 
-#cp "assign4_19200704.sh" "$2"
-#pwd
+
+
+#________________________MAIN CODE________________________#
+
+
+# display script info
+if [ "$#" == 1 ] && [ "$1" == "-i" ] ; then
+    echo "Information: $INFO"
+    exit 1
+fi
+if [ "$#" != 2 ] 
+then
+    echo "Invalid arguments"
+    exit 1;
+fi
+
+#Check that the source directory is valid
+if [ ! -d $1 ]; then
+    echo "Invalid source directory: $1"
+    exit -1
+fi
+# Check that the destination directory is valid
+if [ ! -d $2 ]; then
+    echo "Invalid destination directory: $2"
+    exit -1
+fi
+
+# loop throught the source folders content
 for file in "$1"/*; do
-    FULLNAME=`echo "${file}" | sed "s/.*\///"`
-    NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
-    EXTENSION=`echo "$file" | cut -d'.' -f2`
-    
     #recursivly look inside inner directories
     if [ -d "$file" ]; then
         searchDir "$file"
     fi
 
-    case  ${EXTENSION} in 
-    "png")
-        echo "PNG FILE : $FULLNAME";
-        cp "$1/$FULLNAME" "$2"
-        ;;
-    
-    *)
-    echo "$FULLNAME"
-    ;;  
-    esac
+    if [ -f "$file" ]; then
+        ConvertAndMoveFile "$file"
+    fi
+
 done
+
+
