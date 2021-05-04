4d6182155d35565e6715b04e0c918d35431edfb6
RubenVanBreda <re.vanbreda@gmail.com>
2021-05-04 11:54:51 +0200

    Copying pngs to destination

--- Assignment 4/assign4_19200704.sh
+++ Assignment 4/assign4_19200704.sh
@@ -9,24 +9,31 @@ then
     exit 1;
 fi
 
-# Check that the source directory is valid
-if [ -d $1 ]; then
-    echo "Invalid source directory: $1"
-    exit -1
-fi
-# Check that the destination directory is valid
-if [ -d $2 ]; then
-    echo "Invalid destination directory: $2"
-    exit -1
-fi
+# # Check that the source directory is valid
+# if [ -d $1 ]; then
+#     echo "Invalid source directory: $1"
+#     exit -1
+# fi
+# # Check that the destination directory is valid
+# if [ -d $2 ]; then
+#     echo "Invalid destination directory: $2"
+#     exit -1
+# fi
 
+#cp "assign4_19200704.sh" "$2"
+#pwd
 for file in "$1"/*; do
-    case  ${file: -4} in 
-    ".png")
-        echo "$file"
+    FULLNAME=`echo "${file}" | sed "s/.*\///" ` 
+    NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
+    EXTENSION=`echo "$file" | cut -d'.' -f2`
+    case  ${EXTENSION} in 
+    "png")
+        echo "PNG FILE : $FULLNAME";
+        cp "$1/$FULLNAME" "$2"
         ;;
     *)
-    echo "unknown";;  
+    echo "$FULLNAME"
+    ;;  
     esac
 done
 
