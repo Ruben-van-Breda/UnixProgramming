510deaee37740146942dbf7aad29ef90ad8c74b0
RubenVanBreda <re.vanbreda@gmail.com>
2021-05-04 12:51:53 +0200

    Copying dir structure to destination

--- Assignment 4/assign4_19200704.sh
+++ Assignment 4/assign4_19200704.sh
@@ -20,20 +20,64 @@ fi
 #     exit -1
 # fi
 
+source=$1
+dest=$2
+
+# Define function
+searchDir() {
+ 		
+ 	
+ 	echo "searching dir : $1";
+ 	for file in "$1"/*; do
+        FULLNAME=`echo "${file}" | sed "s/.*\///" ` 
+        NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
+        EXTENSION=`echo "$file" | cut -d'.' -f2`
+        
+        THEPATH="${1/$source}";
+
+       echo "Path : $THEPATH"
+        #recursivly look inside inner directories
+        if [ -d "$file" ]; then
+            searchDir "$file"
+        fi
+
+        case  ${EXTENSION} in 
+        "png")
+           # echo "PNG FILE : $PATH"
+            echo "$1/$FULLNAME -> " "$dest$THEPATH/$FULLNAME"
+            mkdir -p "$dest$THEPATH/" 
+            cp "$1/$FULLNAME" "$dest$THEPATH/$FULLNAME"
+            ;;
+        
+        *)
+        echo "$FULLNAME"
+        ;;  
+    esac
+done
+ 		# Return returns an exit status number between 0 - 255
+ 		return
+}
+
 #cp "assign4_19200704.sh" "$2"
 #pwd
 for file in "$1"/*; do
-    FULLNAME=`echo "${file}" | sed "s/.*\///" ` 
+    FULLNAME=`echo "${file}" | sed "s/.*\///"`
     NAME=`echo "${FULLNAME}" | cut -d'.' -f1`  #To remove the extension you can use
     EXTENSION=`echo "$file" | cut -d'.' -f2`
+    
+    #recursivly look inside inner directories
+    if [ -d "$file" ]; then
+        searchDir "$file"
+    fi
+
     case  ${EXTENSION} in 
     "png")
         echo "PNG FILE : $FULLNAME";
         cp "$1/$FULLNAME" "$2"
         ;;
+    
     *)
     echo "$FULLNAME"
     ;;  
     esac
 done
-
